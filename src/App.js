709da0f4ad4f63c44fc5a89643f1c09b117dafae
samuel1212703 <37654743+samuel1212703@users.noreply.github.com>
2022-10-06 03:09:18 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,256 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useState } from "react";
+import { championList } from "./utils/champion-list";
+
+const appName = "LolChestGranted";
+const riotAPI = "https://euw1.api.riotgames.com/lol/";
+const champIconsPath = "./images/champion-squares-compressed/";
+const api_key = "RGAPI-ac51e2dc-0804-465c-ac9f-5d312b6ddb5f";
 
 function App() {
+  // Values
+  const [data, setData] = useState([]);
+  const [username, setUsername] = useState("");
+  const [accountId, setAccountId] = useState("");
+  const [imageSize, setImageSize] = useState("60px");
+  const [searchFilter, setSearchFilter] = useState("");
+  const [chestGrantedFilter, setChestGrantedFilter] = useState(1);
+  const [page, setPage] = useState("home");
+
+  // Stylings
+  const championImageStyling = {
+    width: imageSize,
+    height: imageSize,
+  };
+
+  const getUserInfo = () => {
+    const header = { "Access-Control-Allow-Origin": "*" };
+    console.log("Getting user information for: " + username);
+    try {
+      axios
+        .get(
+          riotAPI +
+            "summoner/v4/summoners/by-name/" +
+            username +
+            "?api_key=" +
+            api_key,
+          { header }
+        )
+        .then((response) => {
+          setAccountId(response.data.id);
+        });
+      axios
+        .get(
+          riotAPI +
+            "champion-mastery/v4/champion-masteries/by-summoner/" +
+            accountId +
+            "?api_key=" +
+            api_key
+        )
+        .then((response) => {
+          setData(response.data);
+          console.log(data);
+          data.forEach((champion, index) => {
+            if (championList[champion.championId] !== undefined) {
+              champion["name"] = championList[champion.championId];
+            } else {
+              console.log("Id of the fucked champ: " + champion.championId);
+            }
+          });
+        });
+    } catch (error) {
+      setAccountId("");
+      setData([]);
+    }
+  };
+
+  const handlePageChange = (pageName) => {
+    setPage(pageName);
+    setSearchFilter("");
+    setChestGrantedFilter(1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" id="navbar">
+        <Container>
+          <Navbar.Brand>{appName}</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => handlePageChange("home")}>Home</Nav.Link>
+            <Nav.Link onClick={() => handlePageChange("about")}>About</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Enter LoL Name"
+              className="me-2"
+              aria-label="Search"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Button variant="outline-success" onClick={() => getUserInfo()}>
+              Search
+            </Button>
+          </Form>
+        </Container>
+      </Navbar>
+      <div>
+        {page === "home" ? (
+          <div>
+            <div>
+              <Container id="sorting-bar" className="d-flex align-items-center">
+                <Row className="d-flex align-items-center">
+                  <Col xs={3}>
+                    <Form className="d-flex">
+                      <Form.Control
+                        type="search"
+                        placeholder="Filter Champions"
+                        className="me-2"
+                        aria-label="Search"
+                        onChange={(e) => setSearchFilter(e.target.value)}
+                      />
+                    </Form>
+                  </Col>
+                  <Col xs={3}>
+                    <p>Image Size: {imageSize}</p>
+                    <Form.Range
+                      onChange={(e) => setImageSize(e.target.value + "px")}
+                    />
+                  </Col>
+                  <Col xs={3}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        className="checkboxes"
+                        onChange={(e) =>
+                          setChestGrantedFilter(!e.target.checked)
+                        }
+                      />
+                      Chest Granted
+                    </label>
+                  </Col>
+                  <Col xs={3}>
+                    <label>
+                      <input type="checkbox" className="checkboxes" />
+                      My Value
+                    </label>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+            <div id="champion-section">
+              <div className="page_split">
+                <div className="image-container">
+                  {data.map((champion) => {
+                    champion["name"] =
+                      championList[champion.championId].toLowerCase();
+                    if (
+                      champion.chestGranted &&
+                      !chestGrantedFilter &&
+                      (searchFilter === "" ||
+                        champion.name.includes(searchFilter.toLowerCase()))
+                    ) {
+                      try {
+                        if (champion.name.includes(searchFilter)) {
+                          return (
+                            <img
+                              key={champion.championId}
+                              style={championImageStyling}
+                              src={require(champIconsPath +
+                                champion.championId +
+                                ".png")}
+                            />
+                          );
+                        }
+                      } catch (e) {
+                        return (
+                          <img
+                            key={champion.championId}
+                            style={championImageStyling}
+                            src={require(champIconsPath +
+                              champion.championId +
+                              ".png")}
+                          />
+                        );
+                      }
+                    }
+                  })}
+                </div>
+              </div>
+              <div className="page_split">
+                <div className="image-container">
+                  {data.map((champion) => {
+                    champion["name"] =
+                      championList[champion.championId].toLowerCase();
+                    if (
+                      (!champion.chestGranted || chestGrantedFilter) &&
+                      (searchFilter === "" ||
+                        champion.name.includes(searchFilter.toLowerCase()))
+                    ) {
+                      try {
+                        if (champion.name.includes(searchFilter)) {
+                          return (
+                            <img
+                              key={champion.championId}
+                              style={championImageStyling}
+                              src={require(champIconsPath +
+                                champion.championId +
+                                ".png")}
+                            />
+                          );
+                        }
+                      } catch (e) {
+                        return (
+                          <img
+                            key={champion.championId}
+                            style={championImageStyling}
+                            src={require(champIconsPath +
+                              champion.championId +
+                              ".png")}
+                          />
+                        );
+                      }
+                    }
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div id="champion-section">
+            <div id="about-page">
+              <h1>About This Tool</h1>
+              <h5>
+                This tool was made to enable LoL-players to check which of their
+                champions has a chest available. This will allow the player to
+                optimize their loot per match.
+                <br />
+                The tool is free, but if you're feeling generous, you can donate
+                to:{" "}
+                <a href="https://paypal.me/TeNoDk" target="_blank">
+                  paypal.me
+                </a>
+              </h5>
+              <br />
+              <p>
+                {appName} is not endorsed by Riot Games and doesn't reflect the
+                views or opinions of Riot Games or anyone officially involved in
+                producing or managing Riot Games properties. Riot Games, and all
+                associated properties are trademarks or registered trademarks of
+                Riot Games, Inc.
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/images/Riot-Games-API-Developer-Assets-master/README.md
@@ -0,0 +1,47 @@
+# Riot-Games-API-Developer-Assets
+Standard Assets Every Riot Games API Developer Needs
+
+### THE ASSETS CURRENTLY INCLUDED IN THIS REPOSITORY:
+
+* [Tier Icons](https://github.com/RiotAPI/Riot-Games-API-Developer-Assets/tree/master/tier-icons) (Rank/Tier & Division Art)
+* [Runes](https://github.com/hunterwodzenski/Riot-Games-API-Developer-Assets/tree/master/runes-icons) (Standard & Unique/Holiday Rune Art)
+* [Regions](https://github.com/hunterwodzenski/Riot-Games-API-Developer-Assets/tree/master/region-selection-art) (Region Transfer Interface Art)
+* [Old Summoner Squares](https://github.com/hunterwodzenski/Riot-Games-API-Developer-Assets/tree/master/old-summoner-squares) (OG Summoner Art)
+* [Champion Mastery Icons](https://github.com/hunterwodzenski/Riot-Games-API-Developer-Assets/tree/master/champion-mastery-icons) (Champion Mastery Ranks Art)
+* [Compressed Champion Squares](https://github.com/hunterwodzenski/Riot-Games-API-Developer-Assets/blob/master/champion-squares-compressed.zip) (Standard Champion Squares)
+* [Champions' Favorite Skin JSON](https://github.com/hunterwodzenski/Riot-Games-API-Developer-Assets/blob/master/most-popular-skins.json) (Champions Paired With Favorite Skin)
+* [Champion Splash Tiles](https://github.com/hunterwodzenski/Riot-Games-API-Developer-Assets/blob/master/champion-splash-tiles.zip) (Champion Splash Squares from Client)
+* [Class Icons](https://github.com/RiotAPI/Riot-Games-API-Developer-Assets/tree/master/class-icons) (Champion Class/Subclass Icons)
+
+
+### DO YOU HAVE A RGAPI DEVELOPER ACCOUNT? ([Riot's *Getting Started* Guide](https://developer.riotgames.com/getting-started.html))
+Create a RGAPI developer account [here](https://developer.riotgames.com/).
+
+After creating account, sign in and read the [API documentation](https://developer.riotgames.com/api-methods/) to learn more.
+
+There are great step-by-step guides and resources available through the Riot Developer Portal.
+
+*You will receive a developer key to begin using the [API Endpoints](https://developer.riotgames.com/api-methods/).*
+
+### API WRAPPERS/LIBRARIES
+Other API developers have created toolkits to make your life easier!
+ 
+A complete list is maintained [here](https://discussion.developer.riotgames.com/articles/61/public-libraries-for-the-riot-games-api.html) on the discussion boards.
+
+
+### DATA DRAGON CDN
+
+There's TONS of assets hosted on Data Dragon! (view complete reference on the [RGAPI page](https://developer.riotgames.com/static-data.html))
+
+### Communities
+
+- We have an active Riot API developer community on Discord! https://discordapp.com/invite/012zEL33LUQLlYIHn
+
+- We have a NEW [Discussions Board](https://discussion.developer.riotgames.com/index.html)! 
+
+- Follow the API team on Twitter [__[Follow]__](https://twitter.com/RiotGamesAPI)
+
+### Contribution
+
+
+### VIEW THE RIOT GAMES [USAGE POLICIES](https://developer.riotgames.com/policies.html)!
